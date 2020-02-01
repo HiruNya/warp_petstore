95915ba81e611fd4f39e2638e9845c3540982577
Hiruna Kethaka Jayamanne <hirunav2@gmail.com>
2020-02-02 00:17:11 +1300

    Serves generated openapi file and updates warp dependency

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,23 @@ mod store;
 mod user;
 mod util;
 
-use warp::Filter;
+use warp::{document::{describe, to_openapi}, Filter};
 
 #[tokio::main]
 async fn main() {
 	let routes = pet::pet().or(store::store()).or(user::user());
 
+	let documentation = to_openapi(describe(&routes));
+
 	#[cfg(not(feature = "serve"))]
-	{
-		use warp::document::{describe, to_openapi};
-		let documentation = to_openapi(describe(routes));
-		println!("{}", serde_json::to_string_pretty(&documentation).unwrap());
-	}
+	println!("{}", serde_json::to_string_pretty(&documentation).unwrap());
 
 	#[cfg(feature = "serve")]
-	warp::serve(routes).run(([127, 0, 0, 1], 8080)).await
+	{
+		use warp::{filters::path::path, reply::json};
+
+		// Lets serve the specification as well at /openapi.json
+		let routes = routes.or(path("openapi.json").map(move || json(&documentation)));
+		warp::serve(routes).run(([127, 0, 0, 1], 8080)).await
+	}
 }
